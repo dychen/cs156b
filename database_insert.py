9c183ecde86ac9cc5c3ec54a1c14beb3257e63e8
Daniel Chen <dychen@caltech.edu>
2012-10-11 18:22:06 -0700

    Added something to create a table and insert all the data into the database. Takes a while to run. First, in the shell run createdb cs156b to make the database. Also, modify the path location so it points to your all.dta file.

---
+++ database_insert.py
@@ -0,0 +1,39 @@
+import psycopg2
+import code
+
+def create_data_table(cursor):
+    sql_cmd = "CREATE TABLE data (user_id integer NOT NULL, movie_id integer NOT NULL, date integer NOT NULL, rating float, index integer, PRIMARY KEY (user_id, movie_id));"
+    cursor.execute(sql_cmd)
+    print "Table data created"
+
+def insert_data_table(cursor):
+    input_filename = 'all.dta'
+    f = open('../mu/' + input_filename, 'r')
+    i = 0
+    sql_cmd = "INSERT INTO data (user_id, movie_id, date, rating) VALUES "
+    insert_values = []
+    for line in f:
+        user_id, movie_id, date, rating = line.split()
+        insert_values.append("(%s, %s, %s, %s)" % (user_id, movie_id, date, rating))
+        i += 1
+        if i % 10000 == 0 and i != 0:
+            cursor.execute(sql_cmd + ','.join(insert_values))
+            insert_values = []
+            print i
+    cursor.execute(sql_cmd + ','.join(insert_values))
+    f.close()
+    print "%i values inserted into table data." % i
+
+def main():
+    conn_str = "host='localhost' dbname='cs156b'"
+    connection = psycopg2.connect(conn_str)
+    cursor = connection.cursor()
+    create_data_table(cursor)
+    insert_data_table(cursor)
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+if __name__ == "__main__":
+    main()
